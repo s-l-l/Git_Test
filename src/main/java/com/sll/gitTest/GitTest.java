07bf8475dda2534d749e28169d9d9edf70ff337b
sll <2714606709@qq.com>
2022-08-08 23:37:44 +0800

    version 2

--- src/main/java/com/sll/gitTest/GitTest.java
+++ src/main/java/com/sll/gitTest/GitTest.java
@@ -7,5 +7,6 @@ package com.sll.gitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好世界！");
+        System.out.println("你好世界 ver1！");
     }
 }
