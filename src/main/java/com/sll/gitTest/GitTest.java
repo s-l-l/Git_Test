8cac06400eba47061140e39e2c32554c3b3f09c9
sll <2714606709@qq.com>
2022-08-08 23:48:07 +0800

    hot-fix update

--- src/main/java/com/sll/gitTest/GitTest.java
+++ src/main/java/com/sll/gitTest/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("你好世界！");
         System.out.println("你好世界 ver1！");
         System.out.println("你好世界 ver2！");
+        System.out.println("你好世界 hot-fix-Update！");
     }
 }
