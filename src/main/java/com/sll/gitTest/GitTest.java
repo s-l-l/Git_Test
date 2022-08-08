ea4ff78b632f1349ae0fc6e7eb170029df5e6bbc
sll <2714606709@qq.com>
2022-08-08 23:27:34 +0800

    FIRST

---
+++ src/main/java/com/sll/gitTest/GitTest.java
@@ -0,0 +1,11 @@
+package com.sll.gitTest;
+
+/**
+ * @Author 石雷雷
+ * @Create 2022-08-08 23:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好世界！");
+    }
+}
